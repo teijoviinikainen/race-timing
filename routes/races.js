d2a25e336d39cc77866f3612eb777f8906aa183f
Teijo Viinikainen <teijo.viinikainen@aavasoft.fi>
2015-03-03 20:21:51 +0200

    Dev

--- routes/races.js
+++ routes/races.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/tuloslaskenta');
+mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL);
 var express = require('express');
 var races = express.Router();
 
